f62bfe098be6a3b21b4ecef544f392dda515102e
vineethkanaparthi <vineeth0025@gmail.com>
2023-12-01 19:05:32 -0700

    Initial Commit

---
+++ PartyExplorer/PartyExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyExplorerApp.swift
+//  PartyExplorer
+//
+//  Created by Vineeth Kanaparthi on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PartyExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
